6473c3e68799bcedceced31b488cbdaccd23f836
irfan <syedirfan7@users.noreply.github.com>
2016-10-19 20:27:36 +0530

    i_d_f_l

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -8,26 +8,69 @@
 	}
 	class Linkedlist{
 		Node firstNode,lastNode;
+		int count=0;
 		Linkedlist(){
 			firstNode=null;
-		}	
+			
+		}
+		public void printNode(int num){
+			int numNode=num;
+			Node n=firstNode;
+			if (num>count){
+				System.out.println("Unable to print the Node "+num+" as there are only "+count+" Nodes here");
+			}else{
+				while(num-1>0){
+					num--;
+					n=n.next;
+				}
+				System.out.println("Node "+numNode+" has "+n.data);
+			}
+			
+		}
+		public void insertAt(int num,int data){
+			int node=num;
+			Node n=firstNode;
+			Node newNode=new Node(data);
+			if ((num<1) ||(num>count)){
+				System.out.println("Try to add the Nodes from 1 to "+count);
+			}else if(num==1){
+				push(data);
+			}//else if (num==count){insertAtlast(data);}
+			else {
+				while(num-1>0){
+					num--;
+					n=n.next;
+				}
+				newNode.next=n;
+				n.prev.next=newNode;
+				newNode.prev=n.prev;
+				n.prev=newNode;
+				count++;
+				System.out.println("A new node with data "+newNode.data+" is added between "+ (node-1) +" and "+node);
+			}
+		}		
 		public void push(int data){
 			Node newNode=new Node(data);
 			if (firstNode==null){
 				lastNode=newNode;
+				count++;
 			}else {
-			firstNode.prev=newNode;}
+			firstNode.prev=newNode;
+			count++;
+			}
 			newNode.next=firstNode;
 			firstNode=newNode;
-			System.out.println(" "+newNode.data+" is pushed into the list now");
+			System.out.println(newNode.data+" is pushed into the list now\nTotal Nodes are "+count);
+			
 		}
 		public void printForw(){
 			System.out.println("Printing the list in forward direction");
 			Node n= firstNode;
 			if (n!=null){
 				while(n!=null){
-				System.out.println(" "+n.data+" ");
+				System.out.println(n.data);
 				n=n.next;
+				
 				}	
 			}
 			else {System.out.println("List is empty");}
@@ -48,17 +91,44 @@
 			if (firstNode==null){//list is empty
 				System.out.println("No Nodes to delete");
 			}	else  if (firstNode.next==null){// only on node ie second node is null
-				System.out.println(firstNode.data+" is popped out");		
+				count--;
+				System.out.println(firstNode.data+" is popped out\nTotal Nodes are "+count);		
 				lastNode=null;
 				firstNode=null;
+				
 			}
 			else{
-				System.out.println(firstNode.data+" is popped out");		
+				count--;
+				System.out.println(firstNode.data+" is popped out\nTotal Nodes are "+count);		
 				firstNode.next.prev=null;
 				firstNode=firstNode.next;
+				
 			}
 			
-		}	 
+		}
+		public void insertAtlast(int data){
+			Node newNode=new Node(data);
+			lastNode.next=newNode;
+			newNode.prev=lastNode;
+			lastNode=newNode;
+			count++;
+			System.out.println("A new node with data "+newNode.data+" is added at the end of the list\nTotal Nodes are "+count);
+		}
+		public void deleteFromlast(){
+			count--;
+			System.out.println("lastNode is removed with data "+lastNode.data+"\nTotal Nodes are "+count);
+			lastNode=lastNode.prev;
+			lastNode.next=null;
+		}
+		/* public void deleteAt(int num){
+			int numNode=num;
+			Node n=firstNode;
+			while(num-1>0){
+				n=n.next;
+				num--;
+			}
+			
+		} */
 	}
 				
 public class DoublyLinkedList {
@@ -73,10 +143,31 @@ public class DoublyLinkedList {
 			linkedlist.push(9);
 			linkedlist.printForw();
 			linkedlist.printBack();
+			System.out.println("Total Number of Nodes= "+linkedlist.count);
+			linkedlist.printNode(3);
 			linkedlist.pop();
 			linkedlist.pop();
 			linkedlist.printForw();
 			linkedlist.printBack();
+			linkedlist.printNode(3);
+			linkedlist.insertAt(2,100);
+			linkedlist.printNode(3);
+			linkedlist.printForw();
+			linkedlist.printBack();
+			linkedlist.insertAtlast(66);
+			linkedlist.printForw();
+			linkedlist.deleteFromlast();
+			linkedlist.printForw();
+			linkedlist.printBack();
+			linkedlist.insertAt(0,55);
+			linkedlist.insertAt(1,88);
+			linkedlist.insertAt(6,54);
+			linkedlist.insertAt(3,56);
+			linkedlist.insertAt(4,45);
+			linkedlist.insertAt(6,54);
+			linkedlist.insertAt(8,54);
+			linkedlist.printForw();
+			linkedlist.printBack(); //88 5 56 45 100 54 4 
 		}
 	
 	}
